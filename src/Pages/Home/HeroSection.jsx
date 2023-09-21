ddd3567981f2460d98cb6908bf3ef492bdd49bd9
MarcusKyung <Kyungmj@gmail.com>
2023-09-20 20:28:51 -0700

    add: hero section and styling

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,5 +1,24 @@
 export default function HeroSection() {
   return (
-    <h1>HeroSection</h1>
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey I'm Marcus</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            WORDS HERE
+            <br />WORDS HERE
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="/img/hero_img.png" alt="hero-img" />
+      </div>
+    </section>
   )
 }
