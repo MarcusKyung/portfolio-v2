b88675b1572ed08f68a3b0e4e4a8c6430afa5d4d
MarcusKyung <Kyungmj@gmail.com>
2023-09-22 22:36:22 -0700

    add: footer/contact components to main index

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -2,6 +2,9 @@ import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
 import MyPortfolio from "../MyPortfolio";
+import Testimonials from "../Testimonials";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
 
 export default function Homescreen() {
   return (
@@ -10,6 +13,9 @@ export default function Homescreen() {
       <MySkills />
       <AboutMe />
       <MyPortfolio />
+      <Testimonials />
+      <ContactMe />
+      <Footer />
     </>
   );
 }
