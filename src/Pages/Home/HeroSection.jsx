f2a9a84ece36a43a5df2ea8397e84d81305b7281
MarcusKyung <Kyungmj@gmail.com>
2023-09-24 20:44:42 -0700

    add: typewriter effect

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,20 +1,28 @@
+import { Link } from "react-scroll"
+import React from "react"
+import Typewriter from "typewriter-effect"
+
 export default function HeroSection() {
   return (
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey I'm Marcus</p>
+          <p className="section--title">Hey, I'm Marcus</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Full Stack</span>{" "}
-            <br />
-            Developer
+            <span className="hero--section-title--color typewriter--effect">
+            <Typewriter
+              options={{
+                strings: ['> Front End Developer.', '> Project Manager.'],
+                autoStart: true,
+                pauseFor: 3000,
+                loop: true,
+              }}
+            />
+            </span>
           </h1>
-          <p className="hero--section-description">
-            WORDS HERE
-            <br />WORDS HERE
-          </p>
+          <br />
         </div>
-        <button className="btn btn-primary">Get In Touch</button>
+        <Link spy={true} smooth={true} offset={-70} duration={500} to="Contact" className="btn btn-primary">Contact Me</Link>
       </div>
       <div className="hero--section--img">
         <img src="/img/hero_img.png" alt="hero-img" />
