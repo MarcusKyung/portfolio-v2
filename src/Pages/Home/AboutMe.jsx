31e265663a5b7733d2a19f733baa53c5f01cd1f3
MarcusKyung <Kyungmj@gmail.com>
2023-09-25 19:37:19 -0700

    update: aboutme photo

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="./img/codesnip.png" alt="About Me" />
+        <img src="./img/codesnip2.png" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
