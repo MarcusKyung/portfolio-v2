b2de62b7f3971bf36ab154f40394746390b5b872
MarcusKyung <Kyungmj@gmail.com>
2023-10-04 20:36:17 -0700

    add: other work accordions

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -1,4 +1,10 @@
+import React from 'react';
+import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel,} from 'react-accessible-accordion';
 import data from "../../data/index.json";
+
+
+
+
 
 export default function MyPortfolio() {
   return (
@@ -99,6 +105,50 @@ export default function MyPortfolio() {
           </div>
         ))}
       </div>
+
+      <div className="portfolio--section--container closed-btn">
+        <Accordion allowZeroExpanded>
+          <AccordionItem>
+            <AccordionItemHeading>
+                <AccordionItemButton className="btn btn-primary accordion-btn">
+                  Other Work
+                </AccordionItemButton>
+            </AccordionItemHeading>
+            <br />
+            <AccordionItemPanel>
+            <div className="portfolio--section--container">
+              {data?.portfolio3?.map((item, index) => (
+                <div key={index} className="portfolio--section--card">
+                  <div className="portfolio--section--card--content">
+                    <div>
+                      <h3 className="portfolio--section--title">{item.title}</h3>
+                      <p className="text-md">{item.description}</p>
+                      <br />
+                      <p className="text-md"><strong>{item.technologies}</strong></p>
+                    </div>
+                    <div className="link--container">
+                      <p className="text-sm portfolio--link">
+                        <a href={item.link} target="_blank" rel="noopener noreferrer">
+                          <button className="btn btn-primary">
+                            View On GitHub
+                            <span>  </span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 19" fill="none">
+                              <path d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z" stroke="currentColor" strokeWidth="2.66667" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                          </button>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </section>
   )
 }
+
+
