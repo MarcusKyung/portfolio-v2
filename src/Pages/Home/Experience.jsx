fd6f0bee4e67db9722927412113dfd27f51af88e
MarcusKyung <Kyungmj@gmail.com>
2023-10-15 21:46:55 -0700

    update: new resume link

--- src/Pages/Home/Experience.jsx
+++ src/Pages/Home/Experience.jsx
@@ -25,7 +25,7 @@ export default function Experience() {
           <h2 className="sections--heading">Experience & Education</h2>
         </div>
         <div>
-          <a href="https://drive.google.com/file/d/1KgSMv5xuSpI5N-ZVunyz19iWZp0WBV93/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+          <a href="https://drive.google.com/file/d/1NxczOcIaxBOPZoU7-9yaYqrhq_ernkZd/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
             <button className="btn btn-github">View My Resume</button>
           </a>
         </div>
